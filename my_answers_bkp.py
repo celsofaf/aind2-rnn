28111f4cd2b692659b1a423611f0939a2939750b
celsofaf <celsofaf@gmail.com>
2018-05-23 21:34:08 -0300

    Underway

--- my_answers_bkp.py
+++ my_answers_bkp.py
@@ -11,33 +11,24 @@ import keras
 def window_transform_series(series, window_size):
     # containers for input/output pairs
     X = []
-    for i in range(len(series) - window_size):
-        X.append(series[i:i+window_size])
-    y = series[window_size:]
-    
+    y = []
+
     # reshape each 
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
     y = np.asarray(y)
     y.shape = (len(y),1)
 
-    return X, y
+    return X,y
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    model = Sequential()
-    model.add(LSTM(5, input_shape=(window_size, 1)))
-    model.add(Dense(1))
-    
-    return model
+    pass
 
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
-    #punctuation = ['!', ',', '.', ':', ';', '?']
-    unwanted = ['(', ')', '&', '$', '%', '*', '@', '/']
-    for char in unwanted:
-        text = text.replace(char, ' ')  #kept "'", 'à', 'â', 'è', 'é', '-' because valid words can arise from them
+    punctuation = ['!', ',', '.', ':', ';', '?']
 
     return text
 
@@ -46,18 +37,6 @@ def window_transform_text(text, window_size, step_size):
     # containers for input/output pairs
     inputs = []
     outputs = []
-    
-    j = 0
-    while j + window_size + 1 < len(text):
-        inputs.append(text[j:j+window_size])
-        outputs.append(text[j+window_size+1])
-        j = j + step_size
-    
-    # reshape each 
-#    inputs = np.asarray(inputs)
-#    inputs.shape = (np.shape(inputs)[0:2])
-#    outputs = np.asarray(outputs)
-#    outputs.shape = (len(outputs),1)
 
     return inputs,outputs
 
